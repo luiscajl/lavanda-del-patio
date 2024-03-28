f21fabde3121973e98ee6e2d92ddbfd28f821a4c
Luis Carlos <luisca_jl@hotmail.com>
2024-03-28 12:25:24 +0100

    Edit action

--- apps/filebot-bot/build.gradle.kts
+++ apps/filebot-bot/build.gradle.kts
@@ -33,8 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.telegram:telegrambots:6.9.7.1")
     compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor ("org.projectlombok:lombok:1.18.32")
-
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
     testImplementation("org.mockito:mockito-junit-jupiter:5.6.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.modelmapper:modelmapper:3.2.0") {
@@ -42,25 +41,6 @@ dependencies {
         exclude(group = "org.testng", module = "testng")
     }
 }
-
-//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-//    kotlinOptions {
-//        jvmTarget = "21"
-//        freeCompilerArgs = listOf("-Amapstruct.defaultComponentModel=spring")
-//    }
-//}
-
-//tasks.withType<JavaCompile> {
-//    options.compilerArgs.add("-parameters")
-//    options.annotationProcessorGeneratedSourcesDirectory.set(file("build/generated/sources/annotationProcessor/java/main"))
-//}
-
-//tasks.named("jib") {
-//    dependsOn("build")
-//    doLast {
-//        // Jib plugin configuration can be done here or inside the jib block directly.
-//    }
-//}
 
 tasks.withType<Test> {
     useJUnitPlatform()
@@ -71,28 +51,3 @@ tasks.named<BootBuildImage>("bootBuildImage") {
     environment = mapOf("BP_NATIVE_IMAGE" to "true")
     environment = mapOf("TZ" to "Europe/Madrid")
 }
-
-
-//jib {
-//    from {
-//        image = "amazoncorretto:21-alpine"
-//        platforms {
-//            platform {
-//                architecture = "amd64"
-//                os = "linux"
-//            }
-//            platform {
-//                architecture = "arm64"
-//                os = "linux"
-//            }
-//        }
-//    }
-//    to {
-//        image = "registry-1.docker.io/lavandadelpatio/filebot-bot"
-//        tags = setOf(version.toString(), "${version}-${System.getenv("TRIMMED_SHA")}", "latest")
-//    }
-//    container {
-//        environment = mapOf("TZ" to "Europe/Madrid")
-//        user = "568:568"
-//    }
-//}
