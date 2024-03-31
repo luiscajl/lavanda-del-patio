2eede52a5e3b5bf73a3d87b188c7b33a818e524a
Luis Carlos <luisca_jl@hotmail.com>
2024-03-31 14:32:05 +0200

    New executor reflection

--- apps/filebot-executor/src/main/java/es/lavanda/filebot/executor/config/AppBeans.java
+++ apps/filebot-executor/src/main/java/es/lavanda/filebot/executor/config/AppBeans.java
@@ -3,6 +3,10 @@ package es.lavanda.filebot.executor.config;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import es.lavanda.lib.common.model.FilebotExecutionIDTO;
+import es.lavanda.lib.common.model.TelegramFilebotExecutionIDTO;
+import es.lavanda.lib.common.model.TelegramFilebotExecutionODTO;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +15,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @Configuration
 @EnableMongoAuditing
 @EnableAutoConfiguration
+@RegisterReflectionForBinding({FilebotExecutionIDTO.class})
 public class AppBeans {
 
     @Bean
