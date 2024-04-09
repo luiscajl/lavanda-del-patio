26202979519bd607cb36cf2692262cbece82a045
Luis Carlos Jiménez López <9429696+luiscajl@users.noreply.github.com>
2024-04-09 06:55:29 +0200

    Actualizar build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ allprojects {
     repositories {
         mavenCentral()
         maven {
-            url = uri("https://maven.pkg.github.com/Lavanda-del-Patio/*") //FIXME: CHANGE TO LUISCAJL
+            url = uri("https://maven.pkg.github.com/luiscajl/*")
             credentials {
                 username = "luiscajl"
                 password = (System.getenv("GITHUB_TOKEN") ?: findProperty("GITHUB_TOKEN")).toString()
