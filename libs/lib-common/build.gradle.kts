277a330351eabcd7a53272b8961525e03cb42eec
Luis Carlos <luisca_jl@hotmail.com>
2024-04-28 18:44:01 +0200

    Fix libcommon

--- libs/lib-common/build.gradle.kts
+++ libs/lib-common/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
+    id("org.springframework.boot") version "3.2.5"
     id("maven-publish")
     id("java-library")
     id("io.freefair.lombok") version "8.6"
 }
-
 group = "es.lavanda"
 version = "0.1.5"
 
@@ -33,12 +33,11 @@ publishing {
         }
     }
 }
-val springBootStarter by extra { "3.2.5" }
 
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:$springBootStarter")
-    implementation("org.springframework.boot:spring-boot-starter-amqp:$springBootStarter")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
+    implementation("org.springframework.boot:spring-boot-starter-amqp:3.2.5")
     implementation("commons-io:commons-io:2.16.0")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.0")
 }
