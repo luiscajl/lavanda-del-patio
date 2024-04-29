079efa833ff82348e475d419d2b44234b34f007a
Luis Carlos <luisca_jl@hotmail.com>
2024-04-29 18:07:54 +0200

    new changes on frontend

--- apps/frontend/src/app/layout/app.menu.component.ts
+++ apps/frontend/src/app/layout/app.menu.component.ts
@@ -24,17 +24,17 @@ export class AppMenuComponent implements OnInit {
                 label: 'Administration',
                 items: [
                     { label: 'Executor', icon: 'pi pi-fw pi-flag-fill', routerLink: ['/admin/executors'] },
-                    { label: 'Reports', icon: 'pi pi-fw pi-file-o', routerLink: ['/admin/reports'] },
-                    { label: 'Sync', icon: 'pi pi-fw pi-sync', routerLink: ['/admin/sync'] },
+                    // { label: 'Reports', icon: 'pi pi-fw pi-file-o', routerLink: ['/admin/reports'] },
+                    // { label: 'Sync', icon: 'pi pi-fw pi-sync', routerLink: ['/admin/sync'] },
                 ]
             },
-            {
-                label: 'Elastictranscoder',
-                items: [
-                    { label: 'Media', icon: 'pi pi-fw pi-database', routerLink: ['/elastictranscoder/media'] },
-                    { label: 'Progress', icon: 'pi pi-fw pi-forward', routerLink: ['/elastictranscoder/progress'] },
-                ]
-            }
+            // {
+            //     label: 'Elastictranscoder',
+            //     items: [
+            //         { label: 'Media', icon: 'pi pi-fw pi-database', routerLink: ['/elastictranscoder/media'] },
+            //         { label: 'Progress', icon: 'pi pi-fw pi-forward', routerLink: ['/elastictranscoder/progress'] },
+            //     ]
+            // }
         ];
     }
 }
