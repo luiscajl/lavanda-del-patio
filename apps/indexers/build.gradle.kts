08b524d0c17947fc441c2b7baf4b5f40c0b1fff6
Luis Carlos <luisca_jl@hotmail.com>
2024-06-19 20:02:31 +0200

    Test

--- apps/indexers/build.gradle.kts
+++ apps/indexers/build.gradle.kts
@@ -2,9 +2,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     java
-    id("org.springframework.boot") version "3.3.0"
+    id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.5"
-    id("org.graalvm.buildtools.native") version "0.10.1"
+    id("org.graalvm.buildtools.native") version "0.10.2"
     id("io.freefair.lombok") version "8.6"
 }
 
