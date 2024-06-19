9f839b3d6fc61d5f6bb965f1e87511af14555292
Luis Carlos <luisca_jl@hotmail.com>
2024-06-19 18:37:58 +0200

    Downgrade graal

--- apps/indexers/build.gradle.kts
+++ apps/indexers/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
-    id("org.graalvm.buildtools.native") version "0.10.2"
+    id("org.graalvm.buildtools.native") version "0.10.1"
     id("io.freefair.lombok") version "8.6"
 }
 
