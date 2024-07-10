c14ad9eef7d67bfc5001bcd45295728868c00013
Luis Carlos <luisca_jl@hotmail.com>
2024-07-10 20:11:18 +0200

    updating code v4

--- apps/indexers/src/main/java/es/lavanda/indexers/service/Wolfmax4KCallerService.java
+++ apps/indexers/src/main/java/es/lavanda/indexers/service/Wolfmax4KCallerService.java
@@ -2,16 +2,17 @@ package es.lavanda.indexers.service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import es.lavanda.indexers.exception.IndexerException;
 import es.lavanda.indexers.model.Index;
+import es.lavanda.indexers.repository.IndexRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,10 +20,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 import es.lavanda.lib.common.model.flaresolverr.input.FlaresolverrIDTO;
 import es.lavanda.lib.common.model.flaresolverr.output.FlaresolverrODTO;
@@ -33,53 +32,117 @@ import es.lavanda.lib.common.model.flaresolverr.output.FlaresolverrODTO;
 public class Wolfmax4KCallerService {
 
     @Value("${flaresolverr.url}")
-    private String flaresolverrUrl;
+    private String FLARESOLVERR_URL;
+
+    @Value("${proxy.host}")
+    private String PROXY_HOST;
+
+    @Value("${proxy.port}")
+    private Integer PROXY_PORT;
+
+
+    private final Wolfmax4kService wolfmax4kService;
 
     private static final String WOLFMAX4K_URL = "https://wolfmax4k.com";
     private static final String DOMAIN_WOLFMAX4K = "WOLFMAX4K";
     private static final String HTTPS = "https:";
+    private static final String[] SHOWS_URLS = {
+            "/series/720p/",
+            "/series/1080p/",
+            "/series/4k-2160p/"
+    };
+    private static final String[] FILMS_URLS = {
+            "/peliculas/bluray-1080p/",
+            "/peliculas/4k-2160p/"
+    };
 
-    public List<Index> getIndexForMainPageV2(String url, Index.Type type, Index.Quality quality, boolean multipleOnAPage) {
+    @Scheduled(fixedDelay = 24, timeUnit = TimeUnit.HOURS)
+    public void updateIndex() {
+        log.info("Updating Wolfmax4k Indexes");
+        //SHOWS
+//        for (String url : SHOWS_URLS) {
+//            getIndexForMainPageV2(WOLFMAX4K_URL + url, Index.Type.TV_SHOW, getQualityFromUrl(url), true);
+//        }
+        //FILMS
+        for (String url : FILMS_URLS) {
+            getIndexForMainPageV2(WOLFMAX4K_URL + url, Index.Type.FILM, getQualityFromUrl(url), false);
+        }
+        log.info("Finish Wolfmax4k Indexes");
+    }
+
+    private Index.Quality getQualityFromUrl(String url) {
+        if (url.contains("720p")) return Index.Quality.HD;
+        if (url.contains("1080p")) return Index.Quality.FULL_HD;
+        return Index.Quality.ULTRA_HD;
+    }
+
+    public void getIndexForMainPageV2(String url, Index.Type type, Index.Quality quality, boolean multipleOnAPage) {
         log.info("Wolfmax4K Type: {} Quality: {} ", type, quality);
-        FlaresolverrIDTO flaresolverrIDTOWolfmax4k = getHtmlResponse(url);
-        Document document = Jsoup.parse(flaresolverrIDTOWolfmax4k.getSolution().getResponse());
-        List<Index> indexes = new ArrayList<>(getNewReleasesV2(document, type, quality, multipleOnAPage));
-        log.info("Finish Wolfmax4K Type: {} Quality: {} ", type, quality);
-        return indexes;
-    }
-
-    private List<Index> getNewReleasesV2(Document document, Index.Type type, Index.Quality quality, boolean multipleOnAPage) {
-        return new ArrayList<>(getIndexFromColLg2(document.getElementsByClass("col-lg-2"), type, quality, multipleOnAPage));
-    }
-
-    private List<Index> getIndexFromColLg2(Elements colLg2Elements, Index.Type type, Index.Quality quality, boolean multipleOnAPage) {
-        List<Index> indexes = new ArrayList<>();
-        for (Element colLg2 : colLg2Elements) {
-            if (colLg2.getElementsByClass("fs-xs text-muted fw-semibold mb-2").isEmpty()) {
-                Element anchor = colLg2.getElementsByTag("a").first();
-                if (anchor != null) {
+        Document document = retryGetHtmlResponse(url, "row row-cols-xxl-8 row-cols-md-4 row-cols-2");
+        Elements elements = document.getElementsByClass("row row-cols-xxl-8 row-cols-md-4 row-cols-2");
+        for (Element rowElement : elements) {
+            List<Element> colLg2Elements = rowElement.getElementsByClass("col-lg-2");
+            for (int i = colLg2Elements.size() - 1; i >= 0; i--) {
+                Element colLg2Element = colLg2Elements.get(i);
+                try {
                     if (multipleOnAPage) {
-                        indexes.addAll(buildIndexForMulti(colLg2, type, quality, anchor.attr("href")));
+                        processShow(colLg2Element, type, quality);
                     } else {
-                        indexes.add(buildIndex(colLg2, type, quality, anchor.attr("href")));
+                        wolfmax4kService.save(buildIndex(colLg2Element, type, quality));
                     }
+                } catch (NoSuchElementException e) {
+                    log.error("Not found element ");
                 }
             }
         }
-        return indexes;
+        log.info("Finish Wolfmax4K Type: {} Quality: {} ", type, quality);
     }
 
 
-    private Index buildIndex(Element colLg2, Index.Type type, Index.Quality quality, String url) {
+    private void processShow(Element colLg2, Index.Type type, Index.Quality quality) {
+        String urlMainShow = HTTPS + colLg2.getElementsByTag("a").first().attr("href");
+        Document documentMainShow = retryGetHtmlResponse(urlMainShow, "row gx-lg-4 gx-0");
+        Element rowAfterLayoutSection = documentMainShow.getElementsByClass("row gx-lg-4 gx-0").getFirst();
+//        String imageUrl = getImageUrl(colLg2.getElementsByClass("img-fluid rounded-1").attr("src"));
+//        String imageBase64 = getImageAsBase64(imageUrl);
+        if (rowAfterLayoutSection != null) {
+            Elements tempSerieElements = rowAfterLayoutSection.getElementsByClass("temp-serie");
+            for (int i = tempSerieElements.size() - 1; i >= 0; i--) {
+                Element tempSerie = tempSerieElements.get(i);
+                for (int j = tempSerie.getElementsByTag("a").size() - 1; j >= 0; j--) {
+                    Element aElement = tempSerie.getElementsByTag("a").get(j);
+                    String nameShow = colLg2.getElementsByTag("h3").text();
+                    wolfmax4kService.save(buildIndexForShow(nameShow, aElement, type, quality));
+                }
+            }
+        }
+    }
+
+    private Index buildIndexForShow(String nameShow, Element aElement, Index.Type type, Index.Quality quality) {
         Index index = new Index();
-        index.setName(colLg2.getElementsByTag("h3").text());
+        index.setName(nameShow);
+        String url = aElement.getElementsByTag("a").first().attr("href");
+        index.setUrl(WOLFMAX4K_URL + url);
+        Document documentIndividual = retryGetHtmlResponse(index.getUrl(), "h3 fw-semibold mb-1");
+        index.setIndexName(documentIndividual.getElementsByClass("h3 fw-semibold mb-1").text());
+        String imageUrl = getImageUrl(documentIndividual.getElementsByClass("img-fluid rounded-1").attr("src"));
+        index.setImage(getImageAsBase64(imageUrl));
+        index.setDomain(DOMAIN_WOLFMAX4K);
+        index.setQuality(quality);
+        index.setCreateTime(new Date());
+        index.setType(type);
+        return index;
+    }
+
+    private Index buildIndex(Element colLg2OrA, Index.Type type, Index.Quality quality) {
+        Index index = new Index();
+        index.setName(colLg2OrA.getElementsByTag("h3").text());
+        String url = colLg2OrA.getElementsByTag("a").first().attr("href");
         index.setUrl(HTTPS + url);
-        FlaresolverrIDTO flaresolverrIDTOIndividual = getHtmlResponse(index.getUrl());
-        Document documentIndividual = Jsoup.parse(flaresolverrIDTOIndividual.getSolution().getResponse());
+        Document documentIndividual = retryGetHtmlResponse(index.getUrl(), "h3 fw-semibold mb-1");
         index.setIndexName(documentIndividual.getElementsByClass("h3 fw-semibold mb-1").text());
         index.setDomain(DOMAIN_WOLFMAX4K);
-        String imageElement = colLg2.getElementsByClass("img-fluid rounded-1").attr("src");
-        String imageUrl = getImageUrl(imageElement);
+        String imageUrl = getImageUrl(colLg2OrA.getElementsByClass("img-fluid rounded-1").attr("src"));
         index.setImage(getImageAsBase64(imageUrl));
         index.setQuality(quality);
         index.setCreateTime(new Date());
@@ -87,65 +150,19 @@ public class Wolfmax4KCallerService {
         return index;
     }
 
-    private List<Index> buildIndexForMulti(Element colLg2, Index.Type type, Index.Quality quality, String url) {
-        List<Index> indexes = new ArrayList<>();
-        String urlMainShow = HTTPS + url;
-        FlaresolverrIDTO flaresolverrIDTOMainShow = getHtmlResponse(urlMainShow);
-        Document documentMainShow = Jsoup.parse(flaresolverrIDTOMainShow.getSolution().getResponse());
-        Element rowAfterLayoutSection = documentMainShow.getElementsByClass("row gx-lg-4 gx-0").first();
-        if (Objects.isNull(rowAfterLayoutSection) || Objects.isNull(rowAfterLayoutSection.getElementsByClass("temp-serie"))) {
-            buildIndex(colLg2, type, quality, url);
-        }
-        String imageElement = colLg2.getElementsByClass("img-fluid rounded-1").attr("src");
-        String imageUrl = getImageUrl(imageElement);
-        String imageBase64 = getImageAsBase64(imageUrl);
-        for (Element tempSerie : rowAfterLayoutSection.getElementsByClass("temp-serie")) {
-            for (Element a : tempSerie.getElementsByTag("a")) {
-                Index index = new Index();
-                index.setName(colLg2.getElementsByTag("h3").text());
-                index.setUrl(WOLFMAX4K_URL + a.attr("href"));
-                index.setIndexName(getIndexName(index.getUrl()));
-                index.setDomain(DOMAIN_WOLFMAX4K);
-                index.setImage(imageBase64);
-                index.setQuality(quality);
-                index.setCreateTime(new Date());
-                index.setType(type);
-                indexes.add(index);
-            }
-        }
-        return indexes;
-    }
-
-    private String getIndexName(String url) {
-        try {
-            String indexNameString = getHtmlResponse(url).getSolution().getResponse();
-            Document documentOneShow = Jsoup.parse(indexNameString);
-            Element indexName = documentOneShow.getElementsByClass("h3 fw-semibold mb-1").first();
-            return indexName.text();
-        } catch (Exception e) {
-            log.error("Exception trying get indexName: {}", e.getLocalizedMessage());
-            return getIndexName(url);
-        }
-    }
-
-    private RestClient configureRestClient() {
-        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
-        return RestClient.create(restTemplate);
-    }
-
     private String getImageUrl(String imageElement) {
-        if (imageElement.contains("wolfmax4k.com")) {
-            return HTTPS + imageElement;
-        } else {
-            return WOLFMAX4K_URL + imageElement;
-        }
+        return imageElement.contains("wolfmax4k.com") ? HTTPS + imageElement : WOLFMAX4K_URL + imageElement;
     }
 
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(60000); // 60 seconds
-        factory.setReadTimeout(120000);   // 120 seconds
-        return factory;
+    private Document retryGetHtmlResponse(String url, String checkContainsClass) {
+        String htmlResponse = getHtmlResponse(url).getSolution().getResponse();
+        Document document = Jsoup.parse(htmlResponse);
+        if (Boolean.FALSE.equals(document.getElementsByClass(checkContainsClass).isEmpty())) {
+            return document;
+        } else {
+            log.error("Not found {} on the result of the url {}", checkContainsClass, url);
+            return retryGetHtmlResponse(url, checkContainsClass);
+        }
     }
 
     public FlaresolverrIDTO getHtmlResponse(String url) {
@@ -155,8 +172,8 @@ public class Wolfmax4KCallerService {
             request.setCmd("request.get");
             request.setUrl(url);
             request.setMaxTimeout(600000);
-            RestClient restClient = configureRestClient();
-            return restClient.post().uri(flaresolverrUrl).body(request).retrieve().body(FlaresolverrIDTO.class);
+            RestTemplate restTemplate = new RestTemplate();
+            return restTemplate.postForObject(FLARESOLVERR_URL, request, FlaresolverrIDTO.class);
         } catch (Exception e) {
             log.error("Exception getting htmlResponse: {}", e.getMessage());
             return getHtmlResponse(url);
@@ -166,12 +183,25 @@ public class Wolfmax4KCallerService {
     @Cacheable("images")
     public String getImageAsBase64(String imageUrl) {
         log.info("Calling to get image from url {} ", imageUrl);
-        try (InputStream in = new URL(imageUrl).openStream()) {
+        InputStream in = null;
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_HOST, PROXY_PORT));
+            URL url = new URL(imageUrl);
+            URLConnection connection = url.openConnection(proxy);
+            in = connection.getInputStream();
             byte[] imageBytes = IOUtils.toByteArray(in);
             return Base64.getEncoder().encodeToString(imageBytes);
         } catch (IOException e) {
             log.error("Error while fetching image as Base64: {}", e.getLocalizedMessage());
             return imageUrl;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    log.error("Error closing InputStream: {}", e.getLocalizedMessage());
+                }
+            }
         }
     }
 }
--- apps/indexers/src/main/java/es/lavanda/indexers/service/Wolfmax4kService.java
+++ apps/indexers/src/main/java/es/lavanda/indexers/service/Wolfmax4kService.java
@@ -1,5 +1,6 @@
 package es.lavanda.indexers.service;
 
+import es.lavanda.indexers.exception.IndexerException;
 import es.lavanda.indexers.model.Index;
 import es.lavanda.indexers.repository.IndexRepository;
 import es.lavanda.lib.common.model.bt4g.Bt4gDTO;
@@ -30,14 +31,6 @@ public class Wolfmax4kService {
 
     private final String DOMAIN_WOLFMAX4K = "WOLFMAX4K";
 
-    private static final String WOLFMAX4K_URL = "https://wolfmax4k.com";
-    private static final String WOLFMAX4K_FILMS_1080P = "/peliculas/bluray-1080p/";
-    private static final String WOLFMAX4K_FILMS_2160P = "/peliculas/4k-2160p/";
-    private static final String WOLFMAX4K_SHOWS_2160P = "/series/4k-2160p/";
-    private static final String WOLFMAX4K_SHOWS_1080P = "/series/1080p/";
-    private static final String WOLFMAX4K_SHOWS_720P = "/series/720p/";
-
-    private final Wolfmax4KCallerServiceV2 wolfmax4kCallerService;
 
     private final IndexRepository indexRepository;
 
@@ -49,32 +42,20 @@ public class Wolfmax4kService {
         }
     }
 
-    @Scheduled(fixedDelay = 24, timeUnit = TimeUnit.HOURS)
-    public void updateIndex() {
-        log.info("Updating Wolfmax4k Indexes");
-        //SHOWS
-        saveList(wolfmax4kCallerService.getIndexForMainPageV2(WOLFMAX4K_URL + WOLFMAX4K_SHOWS_720P, Index.Type.TV_SHOW, Index.Quality.HD, true));
-        saveList(wolfmax4kCallerService.getIndexForMainPageV2(WOLFMAX4K_URL + WOLFMAX4K_SHOWS_1080P, Index.Type.TV_SHOW, Index.Quality.FULL_HD, true));
-        saveList(wolfmax4kCallerService.getIndexForMainPageV2(WOLFMAX4K_URL + WOLFMAX4K_SHOWS_2160P, Index.Type.TV_SHOW, Index.Quality.ULTRA_HD, true));
-        //FILM
-        saveList(wolfmax4kCallerService.getIndexForMainPageV2(WOLFMAX4K_URL + WOLFMAX4K_FILMS_1080P, Index.Type.FILM, Index.Quality.FULL_HD, false));
-        saveList(wolfmax4kCallerService.getIndexForMainPageV2(WOLFMAX4K_URL + WOLFMAX4K_FILMS_2160P, Index.Type.FILM, Index.Quality.ULTRA_HD, false));
-        log.info("Finish Wolfmax4k Indexes");
-    }
-
-    private void saveList(List<Index> indexes) {
-        for (Index index : indexes) {
-            try {
-                log.debug("Checking if exist index {}", index);
-                if (Boolean.FALSE.equals(indexRepository.existsByIndexName(index.getIndexName()))) {
-                    log.info("Trying to save new index {}", index);
-                    indexRepository.save(index);
-                }
-            } catch (Exception e) {
-                log.error("Can't save object by:", e);
+    public Index save(Index index) {
+        try {
+            log.debug("Checking if exist index {}", index);
+            if (Boolean.FALSE.equals(indexRepository.existsByIndexName(index.getIndexName()))) {
+                log.info("Trying to save new index {}", index);
+                return indexRepository.save(index);
             }
+            return null;
+        } catch (Exception e) {
+            log.error("Can't save object by:", e);
+            throw new IndexerException("Can't save object on database");
         }
     }
+
 
     public void deleteById(String id) {
         indexRepository.deleteById(id);
