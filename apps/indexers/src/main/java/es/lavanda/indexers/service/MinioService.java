a0c38cff8edd947ac5b90d2d7620f7bbaa413f41
Luis Carlos <luisca_jl@hotmail.com>
2024-08-08 11:28:29 +0200

    Update minioservice

--- apps/indexers/src/main/java/es/lavanda/indexers/service/MinioService.java
+++ apps/indexers/src/main/java/es/lavanda/indexers/service/MinioService.java
@@ -13,6 +13,23 @@ import org.springframework.stereotype.Service;
 import java.io.ByteArrayInputStream;
 import java.util.Base64;
 
+import es.lavanda.indexers.exception.IndexerException;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.StatObjectArgs;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -42,19 +59,27 @@ public class MinioService {
     }
 
     public String saveImage(String name, String imageBase64) {
-        if (imageBase64.startsWith("http")) {
-            log.info("Can't save image on MinIO because the image is on http format: {}", imageBase64);
-            throw new IndexerException(String.format("Can't save image on MinIO because the image is on http format: %s", imageBase64));
-        }
-
-        log.info("Saving image on MinIO with name {}", name);
-
         byte[] imageBytes;
-        try {
-            imageBytes = Base64.getDecoder().decode(imageBase64);
-        } catch (IllegalArgumentException e) {
-            log.error("Can't decode this image {}", imageBase64, e);
-            throw new IndexerException(String.format("Can't decode this image %s", imageBase64), e);
+
+        if (imageBase64.startsWith("http") && imageBase64.contains(" ")) {
+            log.info("Downloading image from URL: {}", imageBase64);
+            try {
+                imageBytes = downloadImageFromUrl(imageBase64);
+            } catch (Exception e) {
+                log.error("Can't download image from URL {}", imageBase64, e);
+                throw new IndexerException(String.format("Can't download image from URL %s", imageBase64), e);
+            }
+        } else if (!imageBase64.startsWith("http")) {
+            log.info("Saving image on MinIO with name {}", name);
+            try {
+                imageBytes = Base64.getDecoder().decode(imageBase64);
+            } catch (IllegalArgumentException e) {
+                log.error("Can't decode this image {}", imageBase64, e);
+                throw new IndexerException(String.format("Can't decode this image %s", imageBase64), e);
+            }
+        } else {
+            log.error("Image is http without spaces and is not needed to upload: {}", imageBase64);
+            return imageBase64;
         }
 
         String objectName = name.replaceAll(" ", "_") + ".jpg";
@@ -69,6 +94,17 @@ public class MinioService {
             }
         }
         return getImageUrl(objectName);
+    }
+
+    private byte[] downloadImageFromUrl(String imageUrl) throws Exception {
+        URL url = new URL(imageUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.connect();
+        try (InputStream inputStream = connection.getInputStream()) {
+            return StreamUtils.copyToByteArray(inputStream);
+        }
     }
 
     private boolean objectExistsInMinio(String objectName) {
